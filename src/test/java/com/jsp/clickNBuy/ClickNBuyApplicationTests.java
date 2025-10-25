3fe43dd8b8499386397fb2a733313d6a6a6895a1
Surya12367 <suryaprakashr193@gmail.com>
2025-10-25 12:38:43 +0530

    CI Check

--- src/test/java/com/jsp/clickNBuy/ClickNBuyApplicationTests.java
+++ src/test/java/com/jsp/clickNBuy/ClickNBuyApplicationTests.java
@@ -1,13 +1,10 @@
 package com.jsp.clickNBuy;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClickNBuyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
